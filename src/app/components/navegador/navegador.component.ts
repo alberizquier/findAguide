1134d3e8e5329a7c3e5dbd785e70545463f20e86
Rodri Candela <rodri11htz@gmail.com>
2019-09-17 18:59:24 +0200

    nav guider

---
+++ src/app/components/navegador/navegador.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navegador',
+  templateUrl: './navegador.component.html',
+  styleUrls: ['./navegador.component.css']
+})
+export class NavegadorComponent implements OnInit {
+
+  show:boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
