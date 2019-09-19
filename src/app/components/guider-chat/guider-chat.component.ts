3efe114cb21873cf48b40a371d27aea14241909f
Andres Onate <and.rews@hotmail.com>
2019-09-19 13:29:40 +0200

    chat,suggestion,profile list

---
+++ src/app/components/guider-chat/guider-chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guider-chat',
+  templateUrl: './guider-chat.component.html',
+  styleUrls: ['./guider-chat.component.scss']
+})
+export class GuiderChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/month-destinations/month-destinations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-month-destinations',
+  templateUrl: './month-destinations.component.html',
+  styleUrls: ['./month-destinations.component.scss']
+})
+export class MonthDestinationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
