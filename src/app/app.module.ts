2814fc396e8b3e2202042585d345b3d81b804927
Alberto Izquierdo <alberizquier@gmail.com>
2019-09-19 17:06:53 +0200

    Cambios app.module nazis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SelectorComponent } from './components/selector/selector.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
-import {GuiderBudgetComponent} from './components/guider-budget/guider-budget.component';
-import {GuiderBudgetHistoricComponent} from './components/guider-budget-historic/guider-budget-historic.component';
-import {SideNavBarComponent} from './components/side-nav-bar/side-nav-bar.component';
-
+import { GuiderBudgetComponent} from './components/guider-budget/guider-budget.component';
+import { GuiderBudgetHistoricComponent} from './components/guider-budget-historic/guider-budget-historic.component';
+import { SideNavBarComponent} from './components/side-nav-bar/side-nav-bar.component';
 import { GuiderListComponent } from './components/guider-list/guider-list.component';
-import { SideNavBarComponent } from './components/side-nav-bar/side-nav-bar.component';
 import { NavegadorComponent } from './components/navegador/navegador.component';
 import { GuiderProfileComponent } from './components/guider-profile/guider-profile.component';
 import { FiltersComponent } from './components/filters/filters.component';
@@ -21,19 +19,14 @@ import { FiltersComponent } from './components/filters/filters.component';
     AppComponent,
     LoginComponent,
     SelectorComponent,
-<<<<<<< HEAD
     RegisterFormComponent,
     GuiderListComponent,
     SideNavBarComponent,
     NavegadorComponent,
     GuiderProfileComponent,
-    FiltersComponent
-=======
-    SideNavBarComponent,
-    RegisterFormComponent,
+    FiltersComponent,
     GuiderBudgetComponent,
     GuiderBudgetHistoricComponent
->>>>>>> ebe527d9c967e7c95f039268fb5f70afa681e92c
   ],
   imports: [
     BrowserModule,
