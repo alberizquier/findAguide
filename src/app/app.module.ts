d584faa04e03894017776f6cc5f40032cae0cfb4
Andres Onate <and.rews@hotmail.com>
2019-09-20 12:05:34 +0200

    fotos y edit profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SelectorComponent } from './components/selector/selector.component';
-import { TestComponent } from './components/test/test.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { GuiderListComponent } from './components/guider-list/guider-list.component';
+import { GuiderProfileComponent } from './components/guider-profile/guider-profile.component';
+import { GuiderEditProfileComponent } from './components/guider-edit-profile/guider-edit-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SelectorComponent,
-    TestComponent,
     RegisterFormComponent,
-    GuiderListComponent
+    GuiderListComponent,
+    GuiderProfileComponent,
+    GuiderEditProfileComponent,
   ],
   imports: [
     BrowserModule,
